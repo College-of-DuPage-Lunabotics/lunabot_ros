51faf07ee02d85a97744670b0e60ef4f10b1eb28
Grayson Arendt <grayson.n.arendt5@gmail.com>
2025-03-31 21:49:15 -0500

    Added steamdeck controller.

--- lunabot_teleop/src/controller_teleop.cpp
+++ lunabot_teleop/src/controller_teleop.cpp
@@ -21,10 +21,7 @@ class ControllerTeleop : public rclcpp::Node
 public:
   ControllerTeleop()
   : Node("motor_controller"), manual_enabled_(true), robot_disabled_(false), left_wheel_motor_(
-      "can0", 1),
-    right_wheel_motor_("can0", 2), lift_actuator_motor_("can0", 3), tilt_actuator_left_motor_(
-      "can0", 4),
-    tilt_actuator_right_motor_("can0", 5)
+      "can0", 1), right_wheel_motor_("can0", 2), lift_actuator_motor_("can0", 3)
   {
     velocity_subscriber_ = create_subscription<geometry_msgs::msg::Twist>(
       "cmd_vel", 10, std::bind(&ControllerTeleop::velocity_callback, this, std::placeholders::_1));
@@ -43,11 +40,13 @@ private:
    */
   void declare_and_get_parameters()
   {
-    declare_parameter("xbox_mode", true);
+    declare_parameter("steam_mode", true);
+    declare_parameter("xbox_mode", false);
     declare_parameter("ps4_mode", false);
     declare_parameter("switch_mode", false);
     declare_parameter("outdoor_mode", false);
 
+    get_parameter("steam_mode", steam_mode_);
     get_parameter("xbox_mode", xbox_mode_);
     get_parameter("ps4_mode", ps4_mode_);
     get_parameter("switch_mode", switch_mode_);
@@ -59,7 +58,9 @@ private:
    */
   void apply_controller_mode()
   {
-    if (xbox_mode_) {
+    if (steam_mode_) {
+      RCLCPP_INFO(get_logger(), "STEAMDECK MODE");
+    } else if (xbox_mode_) {
       RCLCPP_INFO(get_logger(), "XBOX MODE");
     } else if (ps4_mode_) {
       RCLCPP_INFO(get_logger(), "PS4 MODE");
@@ -81,13 +82,19 @@ private:
       robot_disabled_ = true;
     }
 
-    lift_actuator_speed_ = (d_pad_vertical_ == 1.0) ? -0.3 : (d_pad_vertical_ == -1.0) ? 0.3 : 0.0;
-    tilt_actuator_speed_ =
-      (right_joystick_y_ > 0.1) ? -0.3 : (right_joystick_y_ < -0.1) ? 0.3 : 0.0;
+    if (!steam_mode_) {
+      lift_actuator_speed_ =
+        (d_pad_vertical_ == 1.0) ? -0.3 : (d_pad_vertical_ == -1.0) ? 0.3 : 0.0;
+    } else {
+      lift_actuator_speed_ =
+        (steam_left_trigger_ == -1.0) ? -0.5 : (steam_right_trigger_ == -1.0) ? 0.5 : 0.0;
+    }
+
+    RCLCPP_INFO(get_logger(), "lift actuator speed: %.2f", lift_actuator_speed_);
 
     speed_multiplier_ = (x_button_ && y_button_) ? 0.3 : (x_button_ ? 0.1 : 0.6);
 
-    if (switch_mode_) {
+    if (switch_mode_ || steam_mode_) {
       left_speed_ = left_joystick_y_ - left_joystick_x_;
       right_speed_ = left_joystick_y_ + left_joystick_x_;
     } else {
@@ -109,8 +116,6 @@ private:
     left_wheel_motor_.SetDutyCycle(std::clamp(left_speed_ * speed_multiplier_, -1.0, 1.0));
     right_wheel_motor_.SetDutyCycle(std::clamp(right_speed_ * speed_multiplier_, -1.0, 1.0));
     lift_actuator_motor_.SetDutyCycle(std::clamp(lift_actuator_speed_, -1.0, 1.0));
-    tilt_actuator_left_motor_.SetDutyCycle(std::clamp(tilt_actuator_speed_, -1.0, 1.0));
-    tilt_actuator_right_motor_.SetDutyCycle(std::clamp(tilt_actuator_speed_, -1.0, 1.0));
   }
 
   /**
@@ -120,17 +125,16 @@ private:
   {
     auto clock = rclcpp::Clock();
 
-    share_button_ = get_button(joy_msg, {9, 8, 9});
-    menu_button_ = get_button(joy_msg, {10, 9, 10});
-    home_button_ = get_button(joy_msg, {11, 10, 8});
-    a_button_ = get_button(joy_msg, {1, 0, 0});
-    b_button_ = get_button(joy_msg, {0, 2, 1});
-    x_button_ = get_button(joy_msg, {2, 3, 2});
-    y_button_ = get_button(joy_msg, {3, 2, 3});
-    left_bumper_ = get_button(joy_msg, {4, 4, 4});
-    right_bumper_ = get_button(joy_msg, {5, 5, 5});
+    share_button_ = get_button(joy_msg, {9, 8, 9, 2});
+    menu_button_ = get_button(joy_msg, {10, 9, 10, 13});
+    home_button_ = get_button(joy_msg, {11, 10, 8, 11});
+    a_button_ = get_button(joy_msg, {1, 0, 0, 3});
+    b_button_ = get_button(joy_msg, {0, 2, 1, 4});
+    x_button_ = get_button(joy_msg, {2, 3, 2, 5});
+    y_button_ = get_button(joy_msg, {3, 2, 3, 6});
+    left_bumper_ = get_button(joy_msg, {4, 4, 4, 20});
+    right_bumper_ = get_button(joy_msg, {5, 5, 5, 21});
 
-    d_pad_horizontal_ = get_axis(joy_msg, {4, 6, 6});
     d_pad_vertical_ = get_axis(joy_msg, {5, 7, 7});
 
     if (share_button_) {
@@ -152,10 +156,13 @@ private:
       left_trigger_ = joy_msg->axes[2];
       right_trigger_ = joy_msg->axes[5];
 
+      steam_left_trigger_ = joy_msg->axes[9];
+      steam_right_trigger_ = joy_msg->axes[8];
+
       if (robot_disabled_) {
         RCLCPP_ERROR(get_logger(), "\033[1;31mROBOT DISABLED\033[0m");
       } else {
-        SparkMax::Heartbeat();
+        lift_actuator_motor_.Heartbeat();
       }
 
       control_robot();
@@ -168,7 +175,7 @@ private:
   void velocity_callback(const geometry_msgs::msg::Twist::SharedPtr velocity_msg)
   {
     if (!manual_enabled_) {
-      SparkMax::Heartbeat();
+      lift_actuator_motor_.Heartbeat();
       double linear_velocity = velocity_msg->linear.x;
       double angular_velocity = velocity_msg->angular.z;
       double wheel_radius = outdoor_mode_ ? 0.2 : 0.127;
@@ -191,7 +198,7 @@ private:
     const sensor_msgs::msg::Joy::SharedPtr & joy_msg,
     const std::initializer_list<int> & mappings)
   {
-    size_t index = xbox_mode_ ? 2 : ps4_mode_ ? 1 : 0;
+    size_t index = steam_mode_ ? 3 : xbox_mode_ ? 2 : ps4_mode_ ? 1 : 0;
     return joy_msg->buttons[*(mappings.begin() + index)];
   }
 
@@ -210,18 +217,17 @@ private:
   rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joystick_subscriber_;
 
   bool manual_enabled_, robot_disabled_;
-  bool xbox_mode_, ps4_mode_, switch_mode_, outdoor_mode_;
-  double speed_multiplier_, left_speed_, right_speed_, lift_actuator_speed_, tilt_actuator_speed_;
+  bool steam_mode_, xbox_mode_, ps4_mode_, switch_mode_, outdoor_mode_;
+  double speed_multiplier_, left_speed_, right_speed_, lift_actuator_speed_;
   bool home_button_, share_button_, menu_button_, a_button_, b_button_, x_button_, y_button_;
   double left_trigger_, right_trigger_, left_bumper_, right_bumper_, d_pad_vertical_,
     d_pad_horizontal_;
+  double steam_left_trigger_, steam_right_trigger_;
   double left_joystick_x_, left_joystick_y_, right_joystick_y_;
 
   SparkMax left_wheel_motor_;
   SparkMax right_wheel_motor_;
   SparkMax lift_actuator_motor_;
-  SparkMax tilt_actuator_left_motor_;
-  SparkMax tilt_actuator_right_motor_;
 };
 
 /**
